9658fb451cfaf652daf83ed1f75846101f649cd0
aodegaa <aodegaard@alaska.edu>
2015-03-11 21:11:56 -0800

    added inventory in shop

--- Assets/Scripts/shoptest.js
+++ Assets/Scripts/shoptest.js
@@ -6,8 +6,13 @@ var foodCount : int = 0;
 var repairKitCount : int = 0;
 var workingSled : boolean = false;
 
+private var windowRect0 = Rect(400, 400, 250, 0);
+
 function OnGUI () 
 {
+windowRect0 = GUILayout.Window( 0, windowRect0, WindowFunction, "Inventory"); 
+
+
 if (GUI.Button (Rect (10,10,200,30), "Buy Dog ($10)") && playerMoney >= 10) 
 {
 playerMoney -= 10;    // Take away some of the player's coins.
@@ -23,7 +28,7 @@ playerMoney -= 1;    // Take away some of the player's coins.
 if (GUI.Button (Rect (10,90,200,30), "Buy Repair Kit ($8)") && playerMoney >= 8) 
 {
 playerMoney -= 8;    // Take away some of the player's coins.
- foodCount += 1;            // Give item to the player scriptness goes here
+repairKitCount += 1;            // Give item to the player scriptness goes here
 }
 
 if (GUI.Button (Rect (10,130,200,30), "Buy Sled ($50)") && playerMoney >= 50) 
@@ -31,4 +36,24 @@ if (GUI.Button (Rect (10,130,200,30), "Buy Sled ($50)") && playerMoney >= 50)
 playerMoney -= 50;    // Take away some of the player's coins.
 workingSled = true;            // Give item to the player scriptness goes here
 }
+}
+function WindowFunction()
+{
+	GUILayout.BeginVertical();
+    
+    GUILayout.BeginHorizontal();
+    GUILayout.Label("Money");
+    GUILayout.Label("Food");
+    GUILayout.Label("Dogs");
+    GUILayout.Label("Repair Kits");
+    GUILayout.EndHorizontal();
+   
+    GUILayout.BeginHorizontal();
+   	GUILayout.Label("$" + playerMoney.ToString());
+   	GUILayout.Label(foodCount.ToString() +" LBS");
+   	GUILayout.Label(dogCount.ToString());
+    GUILayout.Label(repairKitCount.ToString());
+   	GUILayout.EndHorizontal();
+   
+    GUILayout.EndVertical();
 }
