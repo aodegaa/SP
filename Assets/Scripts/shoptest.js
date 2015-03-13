ed0a5f02555c60f6417782ee400d0db1d1c46c9d
Wheels37 <scott.wheeler.35@gmail.com>
2015-03-12 21:26:27 -0600

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	Assets/Scripts/shoptest.js
    	Library/CurrentLayout.dwlt
    	Library/ScriptAssemblies/Assembly-UnityScript.dll
    	Library/ScriptAssemblies/Assembly-UnityScript.dll.mdb
    	Library/assetDatabase3
    	Library/guidmapper
    	Library/metadata/3a/3a90939ea84afc0478f51d86368029fc
    	Library/metadata/52/527f23740ce20d044928766f36c7c91d
    	SP-csharp.sln
    	SP.sln
    	SP.userprefs

--- Assets/Scripts/shoptest.js
+++ Assets/Scripts/shoptest.js
@@ -6,29 +6,51 @@ var foodCount : int = 0;
 var repairKitCount : int = 0;
 var workingSled : boolean = false;
 var render : boolean = false;
+var showSledWarning : boolean = false;
+var dogs = new Array();
 
-private var windowRect0 = Rect(400, 400, 250, 0);
+class Doge{
+	var hungerLevel : int;
+	var health : int;
+	var fatigue : int;
+	var dogName : String;
+}
+
+
+private var inventoryWindow = Rect(400, 400, 250, 0);
 
 function DoMyWindow (windowID : int) {
-	if (GUI.Button (Rect (100,20,100,25), "OK"))
+	if (GUI.Button (Rect (100,20,100,25), "OK")){
 		render = false;
+		showSledWarning = false;
+		}
 }
+
 
 function OnGUI () 
 {
-
-windowRect0 = GUILayout.Window( 0, windowRect0, WindowFunction, "Inventory");
+// inventory window declaration
+inventoryWindow = GUILayout.Window( 0, inventoryWindow, WindowFunction, "Inventory");
 
 var screenWidth = Screen.width;
 var screenHeight = Screen.height;
-var windowRect : Rect = Rect (screenWidth/2 -150, screenHeight/2 -25, 300, 50);
+var sledDogWindow : Rect = Rect (screenWidth/2 -150, screenHeight/2 -25, 300, 50);
+var sledWindow : Rect = Rect (screenWidth/2 -150, screenHeight/2 -25, 300, 50);
 
+
+// show the warning window
 if(render){
-	windowRect = GUI.Window (1, windowRect, DoMyWindow, "You must buy a sled and at least one dog first!");
+	sledDogWindow = GUI.Window (1, sledDogWindow, DoMyWindow, "You must buy a sled and at least one dog first!");
 }
+// show the other warning window
+if(showSledWarning){
+	sledWindow = GUI.Window (2, sledWindow, DoMyWindow, "You may only purchase one sled.");
+}
+
 
 if (GUI.Button (Rect (10,10,200,30), "Buy Dog ($10)") && playerMoney >= 10) 
 {
+dogs.push(new Doge());
 playerMoney -= 10;    // Take away some of the player's coins.
  dogCount += 1;            // Give item to the player scriptness goes here
 }
@@ -53,8 +75,13 @@ repairKitCount += 1;            // Give item to the player scriptness goes here
 
 if (GUI.Button (Rect (10,130,200,30), "Buy Sled ($50)") && playerMoney >= 50) 
 {
+if (workingSled){
+	showSledWarning = true;
+}
+else{
 playerMoney -= 50;    // Take away some of the player's coins.
 workingSled = true;            // Give item to the player scriptness goes here
+}
 }
 }
 
