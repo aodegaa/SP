452324d3fbddf698bfc3d2087f154f11760d1be8
Wheels37 <scott.wheeler.35@gmail.com>
2015-03-17 20:54:27 -0600

    overlay window tests
    Added some classes to test the overlay and tabs

---
+++ Assets/Scripts/TestSceneScript.js
@@ -0,0 +1,6 @@
+﻿#pragma strict
+
+
+function OnGUI(){
+	WindowScript.windowRect = GUILayout.Window(0, WindowScript.windowRect, WindowScript.createManageWindow, "Manage Window");
+}
---
+++ Assets/Scripts/WindowScript.js
@@ -0,0 +1,29 @@
+﻿#pragma strict
+
+static var windowRect : Rect= Rect(0, 0,Screen.width,Screen.height);
+
+
+static function createManageWindow(){
+	
+	
+	GUILayout.BeginVertical();
+	// begin tab area
+	GUILayout.BeginHorizontal();
+	var button1clicked : boolean = GUILayout.Button("option1");
+	if(button1clicked){
+		// show option 1
+	}
+	if(GUILayout.Button("option2")){
+		// show option 2
+	}
+	if(GUILayout.Button("option3")){
+		// show option 3
+	}
+	if(GUILayout.Button("option4")){
+		// show option 4
+	}
+	GUILayout.EndHorizontal();
+	// end tab area
+	
+	GUILayout.EndVertical();
+}
