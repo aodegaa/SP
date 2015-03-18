e21e9933c9af6d3339cf3e12ca979acdd53b72bb
Wheels37 <scott.wheeler.35@gmail.com>
2015-03-17 22:31:29 -0600

    updated window tests
    added tabbing to window tests.

--- Assets/Scripts/TestSceneScript.js
+++ Assets/Scripts/TestSceneScript.js
@@ -1,6 +1,51 @@
-﻿#pragma strict
+#pragma strict
+var myWindow : Rect = Rect(0,0,960, 50);
+var overlayWindow : Rect = Rect(0,50,960,550);
+var renderTab1 : boolean = false;
+var renderTab2 : boolean = false;
+var renderTab3 : boolean = false;
+var renderTab4 : boolean = false;
 
 
 function OnGUI(){
-	WindowScript.windowRect = GUILayout.Window(0, WindowScript.windowRect, WindowScript.createManageWindow, "Manage Window");
+	myWindow =  GUILayout.Window(0, myWindow, WindowScript.createManageWindow, "Manage Window");
+	overlayWindow = GUILayout.Window(1, overlayWindow, WindowScript.createEmptyWindow, "");
+	if(renderTab1){
+		overlayWindow = GUILayout.Window(1,overlayWindow, WindowScript.createManageWindowStatsTab, "Tab 1");
+	}
+	if(renderTab2){
+		overlayWindow = GUILayout.Window(1,overlayWindow, WindowScript.createManageWindowStatsTab, "Tab 2");
+	}
+	if(renderTab3){
+		overlayWindow = GUILayout.Window(1,overlayWindow, WindowScript.createManageWindowStatsTab, "Tab 3");
+	}
+	if(renderTab4){
+		overlayWindow = GUILayout.Window(1,overlayWindow, WindowScript.createManageWindowStatsTab, "Tab 4");
+	}
+
+	if(WindowScript.button1clicked){
+		renderTab1=true;
+		renderTab2=false;
+		renderTab3=false;
+		renderTab4=false;
+	}
+	if(WindowScript.button2clicked){
+		renderTab1=false;
+		renderTab2=true;
+		renderTab3=false;
+		renderTab4=false;
+	}
+	if(WindowScript.button3clicked){
+		renderTab1=false;
+		renderTab2=false;
+		renderTab3=true;
+		renderTab4=false;
+	}
+	if(WindowScript.button4clicked){
+		renderTab1=false;
+		renderTab2=false;
+		renderTab3=false;
+		renderTab4=true;
+	}
+
 }
--- Assets/Scripts/WindowScript.js
+++ Assets/Scripts/WindowScript.js
@@ -1,29 +1,38 @@
 ﻿#pragma strict
 
 static var windowRect : Rect= Rect(0, 0,Screen.width,Screen.height);
+static var button1clicked : boolean;
+static var button2clicked : boolean;
+static var button3clicked : boolean;
+static var button4clicked : boolean;
+
 
 
 static function createManageWindow(){
 	
-	
 	GUILayout.BeginVertical();
+	
 	// begin tab area
 	GUILayout.BeginHorizontal();
-	var button1clicked : boolean = GUILayout.Button("option1");
-	if(button1clicked){
-		// show option 1
-	}
-	if(GUILayout.Button("option2")){
-		// show option 2
-	}
-	if(GUILayout.Button("option3")){
-		// show option 3
-	}
-	if(GUILayout.Button("option4")){
-		// show option 4
-	}
+	button1clicked = GUILayout.Button("option1");
+	button2clicked = GUILayout.Button("option2");
+	button3clicked = GUILayout.Button("option3");
+	button4clicked = GUILayout.Button("option4");
 	GUILayout.EndHorizontal();
 	// end tab area
 	
+	GUILayout.EndVertical();
+}
+
+static function createManageWindowStatsTab(){
+	GUILayout.BeginVertical();
+	GUILayout.Label("health: 100");
+	GUILayout.Label("hunger: 12");
+	GUILayout.Label("fatigue: 2");
+	GUILayout.EndVertical();
+}
+
+static function createEmptyWindow(){
+	GUILayout.BeginVertical();
 	GUILayout.EndVertical();
 }
