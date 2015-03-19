f0769c67b9a388c118a1cd0712d0d5564803bab8
aodegaa <aodegaard@alaska.edu>
2015-03-18 22:47:50 -0800

    Title.
    Removed static images in favor of drawn gui.
    
    Title Scene is updated.
    Delete is for testing purposes. Deletes all PlayerPrefs.
    Continue returns player to last scene they have saved in PlayerPrefs.
    New Game prompts a warning if PlayerPrefs exist.

---
+++ Assets/Scripts/Title.js
@@ -0,0 +1,91 @@
+﻿#pragma strict
+
+var renderNewGameWarning : boolean = false;
+var drawButtons : boolean = true;
+
+function OnGUI()
+{
+	if (drawButtons)
+	{
+	
+		// For Testing Purposes - Delete Later
+		if (GUI.Button(Rect((Screen.width/2 - 340), (Screen.height/2), 80, 50), GUIContent("Delete")))
+		{
+			PlayerPrefs.DeleteAll();
+			return;
+		}
+		// For Testing Purposes - Delete Later
+	
+		// Continue
+		if (GUI.Button(Rect((Screen.width/2 - 340), (Screen.height - 60), 80, 50), GUIContent("Continue")))
+		{
+			Application.LoadLevel(PlayerPrefs.GetInt("PreviousScene"));
+			return;
+		}
+	
+		// Options
+		if (GUI.Button(Rect((Screen.width/2 + 260), (Screen.height - 60), 80, 50), GUIContent("Options")))
+		{
+			//
+			// Options Overlay Window
+			//
+		}
+	
+		// New Game
+		if (GUI.Button(Rect((Screen.width/2 - 40), (Screen.height - 60), 80, 50), GUIContent("New Game")))
+		{
+			if (PlayerPrefs.HasKey("PreviousScene"))
+			{
+				renderNewGameWarning = true;
+				drawButtons = false;
+			}
+		
+			else
+			{
+				PlayerPrefs.DeleteAll();
+				PlayerPrefs.SetInt("PreviousScene", Application.loadedLevel);
+				Application.LoadLevel(PlayerPrefs.GetInt("PreviousScene") + 1);
+				return;
+			}
+		}
+	}
+	
+	// New Game Warning
+	if (renderNewGameWarning)
+	{
+		var newGamePopUpWarning : Rect = Rect((Screen.width/2 - 140), (Screen.height/2 - 50), 280, 100);
+		newGamePopUpWarning = GUI.Window(0, newGamePopUpWarning, NewGameWarningWindow, "Warning");
+	}
+}
+
+// Window Function
+function NewGameWarningWindow()
+{
+	GUILayout.BeginVertical();
+	
+	GUILayout.BeginHorizontal();
+	GUILayout.Label("All previously saved data will be deleted. \nThis cannot be undone.");
+	GUILayout.EndHorizontal();
+	
+	GUILayout.BeginHorizontal();
+	GUILayout.EndHorizontal();
+	
+	GUILayout.BeginHorizontal();
+	if (GUILayout.Button("Submit"))
+	{
+			PlayerPrefs.DeleteAll();
+			PlayerPrefs.SetInt("PreviousScene", Application.loadedLevel);
+			renderNewGameWarning = false;
+			Application.LoadLevel(PlayerPrefs.GetInt("PreviousScene") + 1);
+			return;
+	}
+	
+	if (GUILayout.Button("Cancel"))
+	{
+		renderNewGameWarning = false;
+		drawButtons = true;
+	}
+	GUILayout.EndHorizontal();
+	
+	GUILayout.EndVertical();
+}
