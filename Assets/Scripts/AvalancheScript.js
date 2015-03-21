ce27804a8da3a1fe60bbfbd25a58d1548df8dcb6
Wheels37 <scott.wheeler.35@gmail.com>
2015-03-21 01:05:21 -0600

    Avalanche mini-game
    created an avalanche mini-game.

---
+++ Assets/Scripts/AvalancheScript.js
@@ -0,0 +1,53 @@
+﻿#pragma strict
+
+var tree : GameObject;
+var rock : GameObject;
+var done : boolean = false;
+
+function crashWindow(){
+	GUILayout.BeginVertical();
+	GUILayout.Label("You Crashed!");
+	GUILayout.EndVertical();
+}
+
+function winWindow(){
+	GUILayout.BeginVertical();
+	GUILayout.Label("You Won!");
+	GUILayout.EndVertical();
+}
+
+function OnGUI(){
+	var popupwindow : Rect = Rect(Screen.width/2-25,Screen.height/2-50,100,50);
+	if(Event.current.type == EventType.layout){
+		if(Musher.collision){
+			popupwindow = GUILayout.Window(0,popupwindow, crashWindow, "");
+		}
+		if(done){
+			popupwindow = GUILayout.Window(0,popupwindow, winWindow, "");
+		}
+	}
+}
+	
+function Start () {
+	// create some trees and rocks at random locations to start
+	// max y value is 4.6
+	// min y value is -4.7
+	rock = GameObject.Find("rock");
+	tree = GameObject.Find("pine tree");
+	for(var i:int=0;i<50;i++){
+		Instantiate(tree, Vector3(Random.Range(5.0,100.0),Random.Range(-4.7,4.6),0), Quaternion.identity);
+		Instantiate(rock, Vector3(Random.Range(5.0,100.0),Random.Range(-4.7,4.6),0), Quaternion.identity);
+	}
+	
+}
+
+function Update () {
+	if(!Musher.collision){
+		if(transform.localPosition.x < 115){
+			transform.localPosition.x += 3 *Time.deltaTime;
+		}
+		else {
+			done=true;
+		}
+	}
+}
---
+++ Assets/Scripts/Musher.js
@@ -0,0 +1,55 @@
+﻿#pragma strict
+
+private var speed: float = 2.0;
+private var scenePosition : float = 0.0;
+var success : boolean;
+static var collision : boolean;
+
+function Start () {
+
+}
+
+function Update () {
+	if(!collision){
+		if(scenePosition<115){
+			scenePosition+=3*Time.deltaTime;
+		}
+		else{
+			success = true;
+		}
+		if(Input.GetButton("Up")){
+			transform.Translate(Vector3(0,speed,0) * Time.deltaTime);
+		} 
+		// move down on down arrow key
+		if(Input.GetButton("Down")){
+			transform.Translate(Vector3(0,speed*-1,0) * Time.deltaTime);
+		} 
+		if(Input.GetButton("Right")){
+			transform.Translate(Vector3(speed*1,0,0) * Time.deltaTime);
+		} 
+		if(Input.GetButton("Left")){
+			transform.Translate(Vector3(speed*-1,0,0) * Time.deltaTime);
+		} 
+		// not moving a direction
+		if(!(Input.GetButton("Left")) || (Input.GetButton("Right"))){
+			transform.position.x += 3*Time.deltaTime;
+		}
+		if(transform.position.y > 4.6){
+			transform.position.y = 4.6;
+		}
+		if (transform.position.y<-4.7){
+			transform.position.y = -4.7;
+		}
+		if(transform.position.x>scenePosition+5){
+			transform.position.x = scenePosition+5;
+		}
+		if(transform.position.x<scenePosition-2){
+			transform.position.x = scenePosition-2;
+		}
+	}
+}
+
+// check for collision
+function OnCollisionEnter2D(coll: Collision2D) {
+	collision = true;
+}
