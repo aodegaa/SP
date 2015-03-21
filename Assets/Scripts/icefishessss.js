f6da7f8f8fdf76ef90092b6444eb1f107e9a517f
aodegaa <aodegaard@alaska.edu>
2015-03-20 21:03:04 -0800

    Ice Fishing WIP

---
+++ Assets/Scripts/icefishessss.js
@@ -0,0 +1,73 @@
+﻿#pragma strict
+
+var playerReel : boolean = false;
+var earlyReel : boolean = false;
+var successReel : boolean = false;
+var targetTime : int;
+var buttonContent : String = "Wait...";
+
+function Start()
+{
+	targetTime = Random.Range(1, 11);
+}
+
+function OnGUI() 
+{
+	var fishingWindow : Rect = Rect(Screen.width/2 - 135, Screen.height/2 -135, 270, 270);
+	fishingWindow = GUI.Window(0, fishingWindow, IceFishing, "");
+}
+
+function IceFishing()
+{
+	var fishingContent = Resources.Load("start");
+
+	if(Time.time < targetTime)
+	{
+		fishingContent = Resources.Load("start");
+		if (playerReel)
+		{
+			earlyReel = true;
+		}
+	}
+	
+	if(Time.time >= targetTime && Time.time <= targetTime + 2)
+	{
+		fishingContent = Resources.Load("moment");
+		buttonContent = "REEL IN!";
+		if (playerReel)
+		{
+			successReel = true;
+		}
+	}
+	
+	if((Time.time > targetTime + 1 && !successReel) || earlyReel)
+	{
+		fishingContent = Resources.Load("rage");
+		buttonContent = "You lose. Play again?";
+	}
+	
+	if(Time.time > targetTime + 1 && successReel && !earlyReel)
+	{
+		fishingContent = Resources.Load("happy");
+		buttonContent = "You win. Play again?";
+	}
+
+	GUILayout.BeginVertical();
+	
+	GUILayout.BeginHorizontal();
+	GUILayout.FlexibleSpace();
+	GUILayout.Label(fishingContent);
+	GUILayout.FlexibleSpace();
+	GUILayout.EndHorizontal();
+	
+	GUILayout.BeginHorizontal();
+	GUILayout.FlexibleSpace();
+	if(GUILayout.Button(buttonContent))
+	{
+		playerReel = true;
+	}
+	GUILayout.FlexibleSpace();
+	GUILayout.EndHorizontal();
+	
+	GUILayout.EndVertical();
+}
