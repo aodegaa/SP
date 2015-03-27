2df7b533acb38a1efda0096f41cd3c873c73004d
Wheels37 <scott.wheeler.35@gmail.com>
2015-03-27 02:19:38 -0600

    major shop cleanup
    cleaned up shoptest in Shop.js. major window logic was moved to
    WindowScript

---
+++ Assets/Scripts/Shop.js
@@ -0,0 +1,127 @@
+﻿#pragma strict
+
+// window declarations
+var inventoryWindow = Rect(Screen.width/2 - 125, Screen.height - 90, 250, 80);
+var thePopUpWindow : Rect = Rect (Screen.width/2 -150, Screen.height/2 -25, 300, 50);
+var thePopUpWindow2 : Rect = Rect(Screen.width/2-150, Screen.height/2-50, 300,100); // popup for tutorial window
+var thePopUpWindow3 : Rect = Rect (250, 100, 200, 200);
+// end window declarations
+
+// booleans to control tabs below 
+var browseWares : boolean = false;
+// end booleans
+
+// additional variables
+var shopMessage : String = "Welcome!";
+
+
+function Start () {
+
+}
+
+function Update () {
+
+}
+
+function OnGUI() {
+	renderTabs();
+}
+
+// renderTabs is used to display the varoius tabs (browse wares, etc)
+// it handles the logic of switching GUI calls.
+// every major if statement handles a full screen. 
+function renderTabs(){
+	if(browseWares){
+		if(!WindowScript.shopClose){
+			var browseWindow : Rect = Rect(0, 0, Screen.width, Screen.height);
+			browseWindow = GUILayout.Window(0, browseWindow, WindowScript.createShopWindow, "Wares");
+		}
+		else{
+			WindowScript.shopClose = false;
+			browseWares=false;
+		}
+	}
+	// the default town layout. will display on startup, and when exiting the other full screen options
+	else{
+		// creates and displays the player's choices in the shop (browse wares, trade, leave)
+		var shopChoicesWindow : Rect = Rect(10, 90, 160, 110);
+		shopChoicesWindow = GUILayout.Window(0, shopChoicesWindow, ShopChoices, "");
+		
+		// creates and displays the shop owner's dialog window
+		var shopDialogWindow : Rect = Rect(Screen.width/2 - 120, 10, 240, 60);
+		shopDialogWindow = GUI.Window(1, shopDialogWindow, ShopDialog, "");
+		
+		// creates and display info on the town (town name, time and date)
+		var townInfoWindow : Rect = Rect(10, 10, 160, 70);
+		townInfoWindow = GUI.Window(2, townInfoWindow, TownInformation, "Undead Burg" + " Store");
+	}
+	
+}
+
+// gives various choices that spawn overlays when clicked
+function ShopChoices()
+{
+	GUILayout.BeginVertical();
+	
+	GUILayout.BeginHorizontal();
+	if(GUILayout.Button("Browse Wares"))
+	{
+		// set flag to display the wares tab
+		browseWares = true;	
+	}
+	GUILayout.EndHorizontal();
+	
+	GUILayout.BeginHorizontal();
+	if(GUILayout.Button("Trades"))
+	{
+		// spawns trade options
+		shopMessage="How do these trades sound?";
+	}
+	GUILayout.EndHorizontal();
+	
+	GUILayout.BeginHorizontal();
+	if(GUILayout.Button("Leave Store"))
+	{
+		// leave the shop
+		shopMessage="Goodbye!";
+	}
+	GUILayout.EndHorizontal();
+	
+	GUILayout.EndVertical();
+}
+
+// town information function used to spawn a small window displaying town name and time
+function TownInformation()
+{
+	var myClock = Resources.Load("clock");
+
+	GUILayout.BeginVertical();
+	
+	GUILayout.BeginHorizontal();
+	GUILayout.FlexibleSpace();
+	GUILayout.Label("June 6th, 1969"); // this needs to be updated to display the date stored in player prefs
+	GUILayout.FlexibleSpace();
+	GUILayout.EndHorizontal();
+	
+	GUILayout.BeginHorizontal();
+	GUILayout.FlexibleSpace();
+	GUILayout.Label(myClock);
+	GUILayout.Label("13:00"); // likewise, update to be based on player prefs
+	GUILayout.FlexibleSpace();
+	GUILayout.EndHorizontal();
+	
+	GUILayout.EndVertical();
+}
+
+// function to create the window for shop dialog
+// dialog is updated based on player interaction within the shop
+function ShopDialog()
+{
+	GUILayout.BeginVertical();
+	GUILayout.BeginHorizontal();
+	GUILayout.FlexibleSpace();
+	GUILayout.Label(shopMessage);
+	GUILayout.FlexibleSpace();
+	GUILayout.EndHorizontal();
+	GUILayout.EndVertical();
+}
--- Assets/Scripts/WindowScript.js
+++ Assets/Scripts/WindowScript.js
@@ -9,7 +9,6 @@ static var button5clicked : boolean;
 static var button6clicked : boolean;
 
 
-
 static function createManageWindow(){
 	
 	GUILayout.BeginVertical();
@@ -188,6 +187,239 @@ static function createOptionsTab(){
 	// finish this one too
 	GUILayout.EndVertical();
 }
+
+// vars associated with the shop
+static var shopWindow : Rect = Rect(0, 0, Screen.width, Screen.height);
+static var playerMoney : int = 1000;
+static var sledPrice : int = 100;
+static var dogPrice : int = 25;
+static var foodPrice : int = 2;
+static var repairkitPrice : int = 15;
+static var fishingpolePrice : int = 30;
+static var baitPrice : int = 1;
+static var gunPrice : int = 30;
+static var bulletsPrice : int = 1;
+static var shopClose : boolean = false;
+// function to spawn the shop window
+static function createShopWindow(){
+	var sledIcon = Resources.Load("sled");
+	var dogIcon = Resources.Load("dog");
+	var foodIcon = Resources.Load("food");
+	var repairkitIcon = Resources.Load("repairkit");
+	var fishingpoleIcon = Resources.Load("fishingpole");
+	var baitIcon = Resources.Load("bait");
+	var gunIcon = Resources.Load("gun");
+	var bulletsIcon = Resources.Load("bullets");
+	if(GUILayout.Button("exit shop")){
+		shopClose = true;
+	}
+
+	GUILayout.BeginHorizontal();
+	GUILayout.FlexibleSpace();
+	GUILayout.BeginVertical();
+	GUILayout.Label("Sled", GUILayout.Width(80));
+	GUILayout.Label(sledIcon, GUILayout.Width(80));
+	GUILayout.Label("A sturdy vehicles pulled by dogs", GUILayout.MaxWidth(120));
+	GUILayout.EndVertical();
+	GUILayout.FlexibleSpace();
+	
+	GUILayout.BeginVertical();
+	GUILayout.Label("Dog", GUILayout.Width(80));
+	GUILayout.Label(dogIcon, GUILayout.Width(80));
+	GUILayout.Label("Dogs bred especially for travel. You must feed them if you want them to perform well.", GUILayout.MaxWidth(120));
+	GUILayout.EndVertical();
+	GUILayout.FlexibleSpace();
+	
+	GUILayout.BeginVertical();
+	GUILayout.Label("Fishing Pole", GUILayout.Width(80));
+	GUILayout.Label(fishingpoleIcon, GUILayout.Width(80));
+	GUILayout.Label("A tool used to catch fish for food. Fishing attempts require bait.", GUILayout.MaxWidth(120));
+	GUILayout.EndVertical();
+	GUILayout.FlexibleSpace();
+	
+	GUILayout.BeginVertical();
+	GUILayout.Label("Gun", GUILayout.Width(80));
+	GUILayout.Label(gunIcon, GUILayout.Width(80));
+	GUILayout.Label("Can be used to get out of dangerous situations. Requires bullets.", GUILayout.MaxWidth(120));
+	GUILayout.EndVertical();
+	GUILayout.FlexibleSpace();
+	
+	GUILayout.EndHorizontal();
+	
+	GUILayout.BeginVertical();
+	GUILayout.BeginHorizontal();
+	
+	GUILayout.FlexibleSpace();
+	
+	if(GUILayout.Button("Buy ($" + sledPrice + ")", GUILayout.Width(120)))
+	{
+		playerMoney -= sledPrice;
+	}
+	
+	GUILayout.FlexibleSpace();
+	
+	if(GUILayout.Button("Buy ($" + dogPrice + ")", GUILayout.Width(120)))
+	{
+		playerMoney -= dogPrice;
+	}
+	
+	GUILayout.FlexibleSpace();
+	
+	if(GUILayout.Button("Buy ($" + fishingpolePrice + ")", GUILayout.Width(120)))
+	{
+		playerMoney -= fishingpolePrice;
+	}
+	
+	GUILayout.FlexibleSpace();
+	
+	if(GUILayout.Button("Buy ($" + gunPrice + ")", GUILayout.Width(120)))
+	{
+		playerMoney -= gunPrice;
+	}
+	GUILayout.FlexibleSpace();
+	
+	GUILayout.EndHorizontal();
+	GUILayout.Label("");
+	GUILayout.Label("");
+	GUILayout.Label("");
+	GUILayout.EndVertical();
+	
+	GUILayout.BeginHorizontal();
+	GUILayout.FlexibleSpace();
+	GUILayout.BeginVertical();
+	GUILayout.Label("Repair Kit", GUILayout.Width(80));
+	GUILayout.Label(repairkitIcon, GUILayout.Width(80));
+	GUILayout.Label("Slightly improves sled health.", GUILayout.MaxWidth(120));
+	GUILayout.EndVertical();
+	GUILayout.FlexibleSpace();
+	
+	GUILayout.BeginVertical();
+	GUILayout.Label("Food", GUILayout.Width(80));
+	GUILayout.Label(foodIcon, GUILayout.Width(80));
+	GUILayout.Label("Prevents hunger. Used by player and dogs.", GUILayout.MaxWidth(120));
+	GUILayout.EndVertical();
+	GUILayout.FlexibleSpace();
+	
+	GUILayout.BeginVertical();
+	GUILayout.Label("Bait", GUILayout.Width(80));
+	GUILayout.Label(baitIcon, GUILayout.Width(80));
+	GUILayout.Label("Used for a fishing attempt. Requires a fishing pole.", GUILayout.MaxWidth(120));
+	GUILayout.EndVertical();
+	GUILayout.FlexibleSpace();
+	
+	GUILayout.BeginVertical();
+	GUILayout.Label("Bullets", GUILayout.Width(80));
+	GUILayout.Label(bulletsIcon, GUILayout.Width(80));
+	GUILayout.Label("Ammunition for a gun. Carry a lot for protection.", GUILayout.MaxWidth(120));
+	GUILayout.EndVertical();
+	GUILayout.FlexibleSpace();
+	
+	GUILayout.EndHorizontal();
+	
+	GUILayout.BeginVertical();
+	GUILayout.BeginHorizontal();
+	
+	GUILayout.FlexibleSpace();
+	
+	if(GUILayout.Button("Buy ($" + repairkitPrice + ")", GUILayout.Width(120)))
+	{
+		playerMoney -= repairkitPrice;
+	}
+	
+	GUILayout.FlexibleSpace();
+	
+	if(GUILayout.Button("Buy ($" + foodPrice + ")", GUILayout.Width(120)))
+	{
+		playerMoney -= foodPrice;
+	}
+	
+	GUILayout.FlexibleSpace();
+	
+	if(GUILayout.Button("Buy ($" + baitPrice + ")", GUILayout.Width(120)))
+	{
+		playerMoney -= baitPrice;
+	}
+	
+	GUILayout.FlexibleSpace();
+	
+	if(GUILayout.Button("Buy ($" + bulletsPrice + ")", GUILayout.Width(120)))
+	{
+		playerMoney -= bulletsPrice;
+	}
+	GUILayout.FlexibleSpace();
+	
+	GUILayout.EndHorizontal();
+	GUILayout.BeginHorizontal();
+	
+	GUILayout.FlexibleSpace();
+	
+	if(GUILayout.Button("Buy 5 ($" + repairkitPrice * 5 + ")", GUILayout.Width(120)))
+	{
+		playerMoney -= repairkitPrice * 5;
+	}
+	
+	GUILayout.FlexibleSpace();
+	
+	if(GUILayout.Button("Buy 5 ($" + foodPrice * 5 + ")", GUILayout.Width(120)))
+	{
+		playerMoney -= foodPrice * 5;
+	}
+	
+	GUILayout.FlexibleSpace();
+	
+	if(GUILayout.Button("Buy 5 ($" + baitPrice * 5 + ")", GUILayout.Width(120)))
+	{
+		playerMoney -= baitPrice * 5;
+	}
+	
+	GUILayout.FlexibleSpace();
+	
+	if(GUILayout.Button("Buy 5 ($" + bulletsPrice * 5 + ")", GUILayout.Width(120)))
+	{
+		playerMoney -= bulletsPrice * 5;
+	}
+	GUILayout.FlexibleSpace();
+	
+	GUILayout.EndHorizontal();
+
+	GUILayout.BeginHorizontal();
+	
+	GUILayout.FlexibleSpace();
+	
+	if(GUILayout.Button("Buy 10 ($" + repairkitPrice * 10 + ")", GUILayout.Width(120)))
+	{
+		playerMoney -= repairkitPrice * 10;
+	}
+	
+	GUILayout.FlexibleSpace();
+	
+	if(GUILayout.Button("Buy 10 ($" + foodPrice * 10 + ")", GUILayout.Width(120)))
+	{
+		playerMoney -= foodPrice * 10;
+	}
+	
+	GUILayout.FlexibleSpace();
+	
+	if(GUILayout.Button("Buy 10 ($" + baitPrice * 10 + ")", GUILayout.Width(120)))
+	{
+		playerMoney -= baitPrice * 10;
+	}
+	
+	GUILayout.FlexibleSpace();
+	
+	if(GUILayout.Button("Buy 10 ($" + bulletsPrice * 10 + ")", GUILayout.Width(120)))
+	{
+		playerMoney -= bulletsPrice * 10;
+	}
+	GUILayout.FlexibleSpace();
+	
+	GUILayout.EndHorizontal();
+	
+	GUILayout.Label("");
+	GUILayout.EndVertical();
+}
+
+
 
 static function createEmptyWindow(){
 	GUILayout.BeginVertical();
