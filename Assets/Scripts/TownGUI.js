94daa0310c105dbffec7bd6c7f6bb884a7ce1c1b
Wheels37 <scott.wheeler.35@gmail.com>
2016-04-23 20:15:40 -0600

    Added map item for leaving town functionality, added check situation overlay
    see comments in function leaveTownFunction() for comments on how to get
    destinations
    overlay added for checking the situation. GUI needs to be updated for
    this portion.
    added checks for button presses from onGUI buttons

--- Assets/Scripts/TownGUI.js
+++ Assets/Scripts/TownGUI.js
@@ -15,6 +15,12 @@
 // default time/date if not set yet
 var townTime : String = "13:00";
 var townDate : String = "June 1st, 1969";
+var map: Map;
+
+
+private var manage : boolean;
+private var showTime : boolean = true;
+private var leaveTown : boolean;
 
 private var townName : String;
 var townNameStyle : GUIStyle;
@@ -28,7 +34,8 @@ var leaveTownButtonStyle : GUIStyle;
 
 function Start() 
 {
-	GetTownName();
+	checkSituation.close=false;
+	GetTownName(); // deprecated. use map.getCurrentCity() 
 	
 	// check for preset time. set the time if it isn't set yet.
 	if(!PlayerPrefs.HasKey("Game Time"))
@@ -36,6 +43,21 @@ function Start()
 		var theTime : gameTime = ScriptableObject.CreateInstance("gameTime") as gameTime;
 		theTime.init(townDate+" "+townTime);
 		PlayerPrefs.SetString("Game Time",theTime.ToString());
+	}
+	
+	// initialize the map
+	map = ScriptableObject.CreateInstance("Map") as Map;
+	// check for a current city
+	if(!PlayerPrefs.HasKey("Current City")){
+		// set the current city to the starting city if none set already
+		PlayerPrefs.SetInt("Current City",1);
+	}
+	// initialize the map with current city.
+	map.init(PlayerPrefs.GetInt("Current City"));
+	// see if we're in the last town.
+	// if we are, the direction needs to be set to 1, indicating the player is now traveling back towards town 1.
+	if(PlayerPrefs.GetInt("Current City")==22){
+		PlayerPrefs.SetInt("Direction", 1);
 	}
 }
 
@@ -48,19 +70,60 @@ function OnGUI()
 {
 	GUI.Label(Rect(350, 10, 100, 100), GUIContent(townName), townNameStyle);
 	
-	// create time window, set to be in top right hand corner of the screen
-	var townWindow : Rect = Rect(Screen.width-120, 0, 120, 65);
-	townWindow = GUI.Window(5, townWindow, TownInformation,"Current Time");
+	
 
 	if (GUI.Button(Rect((Screen.width - 700), (Screen.height - 40), 60, 50), GUIContent(manageButtonText), manageButtonStyle))
 	{
-
+		Debug.Log("Check the Situation");
+		manage=true;
 	}
 
 	if (GUI.Button(Rect((Screen.width - 260), (Screen.height - 40), 60, 50), GUIContent(leaveTownButtonText), leaveTownButtonStyle))
 	{
-
+		Debug.Log("Leave town");
+		leaveTown=true;
 	}
+	renderGUI();
+}
+
+function renderGUI(){
+	if(manage){
+		showTime=false;
+		// every time you spawn the check situation window, you must include the following checks
+		if(!checkSituation.close){
+			checkSituation.ManageWindow();
+		}
+		else{
+			// checkSituation.close=false must be called every time the window is closed.
+			checkSituation.close=false;
+			manage=false;
+			showTime=true;
+		}
+	}
+	if(leaveTown){
+	
+	}
+	if(showTime){
+		// create time window, set to be in top right hand corner of the screen
+		var townWindow : Rect = Rect(Screen.width-120, 0, 120, 65);
+		townWindow = GUI.Window(5, townWindow, TownInformation,"Current Time");
+	}
+}
+
+// used to display options to players when leaving the town.
+function leaveTownFunction(){
+	// check to see if the player has a choice on where to go next
+		if(map.getCityByID(map.currentCity).getDestinations().length>1){
+			// this means there are more than 1 posible destination cities.
+			// loop gets them one by one.
+			for(var path : Path in map.getCityByID(map.currentCity).getDestinations()) {
+				// path now references an object of type path that contains the destination name and the distance to that city
+				// these can be accessed via:
+				// path.getDestinationStsring() which will give you the destination city as a string and
+				// path.getDistance() which will give you the distance to the destination city as an int
+			}
+		}
+
 }
 
 // used to display the date and time, with an image of a clock
---
+++ Assets/Scripts/checkSituation.js
@@ -0,0 +1,389 @@
+﻿#pragma strict
+static var close: boolean=false;
+static var showPlayerTab : boolean;
+static var showDogsTab : boolean;
+static var showInventoryTab : boolean;
+static var showOptionsTab : boolean;
+
+
+static function ManageWindow(){
+	var manageWindowOverlayRect : Rect = Rect(0,0,Screen.width,50); // for the different options buttons that spawn the tabs
+	var manageWindowTabsRect : Rect = Rect(0,50,Screen.width,Screen.height-50); // to display the required tab
+	
+	manageWindowOverlayRect=GUILayout.Window(0,manageWindowOverlayRect,ManageWindowOverlay,"");
+	
+	
+	if(showPlayerTab){
+		manageWindowTabsRect = GUILayout.Window(1,manageWindowTabsRect,PlayerTab,"");
+	}
+	else if(showDogsTab){
+		manageWindowTabsRect = GUILayout.Window(1,manageWindowTabsRect,DogsTab,"");
+	}
+	else if(showInventoryTab){
+		manageWindowTabsRect = GUILayout.Window(1,manageWindowTabsRect,InventoryTab,"");
+	}
+	else if(showOptionsTab){
+		manageWindowTabsRect = GUILayout.Window(1,manageWindowTabsRect,OptionsTab,"");
+	}
+	else {
+		manageWindowTabsRect = GUILayout.Window(1,manageWindowTabsRect,emptyWindow,"");
+	}
+}
+
+static function ManageWindowOverlay(){
+	GUILayout.BeginVertical();
+	
+	GUILayout.BeginHorizontal();
+	if(showPlayerTab)
+	{
+		GUI.enabled=false;
+	}
+	if(GUILayout.Button("Player")){
+		showPlayerTab=true;
+		showDogsTab=false;
+		showInventoryTab=false;
+		showOptionsTab=false;
+	}
+	GUI.enabled=true;
+	
+	if(showDogsTab)
+	{
+		GUI.enabled=false;
+	}
+	if(GUILayout.Button("Dogs")){
+		showDogsTab=true;
+		showPlayerTab=false;
+		showInventoryTab=false;
+		showOptionsTab=false;
+		
+		
+	}
+	GUI.enabled=true;
+	
+	if(showInventoryTab)
+	{
+		GUI.enabled=false;
+	}
+	if(GUILayout.Button("Inventory")){
+		showInventoryTab=true;
+		showDogsTab=false;
+		showPlayerTab=false;
+		showOptionsTab=false;
+		
+		
+	}
+	GUI.enabled=true;
+	
+	if(showOptionsTab)
+	{
+		GUI.enabled=false;
+	}
+	if(GUILayout.Button("Options")){
+		showOptionsTab=true;
+		showDogsTab=false;
+		showInventoryTab=false;
+		showPlayerTab=false;
+	}
+	GUI.enabled=true;
+	
+	if(GUILayout.Button("X", GUILayout.Width(25)))
+	{
+		close=true;
+	}
+	// need to have the endHorizontal label here in case it isn't called on a previous button press.
+	// also don't want to double end it if a button is pressed
+	GUILayout.EndHorizontal();
+	
+	GUILayout.EndVertical();
+}
+
+static function PlayerTab()
+{
+	var playerName : String = PlayerPrefs.GetString("PlayerName");
+	var playerHealth : int = PlayerPrefs.GetInt("PlayerHealth");
+	var playerHunger : int = PlayerPrefs.GetInt("PlayerHunger");
+	var playerFatigue : int = PlayerPrefs.GetInt("PlayerFatigue");
+	var difficultyChoice : String = PlayerPrefs.GetString("Difficulty");
+	var healthyPopulation : int = PlayerPrefs.GetInt("HealthyPopulation");
+	var sickPopulation : int = PlayerPrefs.GetInt("SickPopulation");
+	var deceasedPopulation : int = PlayerPrefs.GetInt("DeceasedPopulation");
+	
+	
+	
+	var playerProfilePicture = Resources.Load("playerprofile");
+	
+	GUILayout.BeginHorizontal();
+	GUILayout.BeginVertical();
+	GUILayout.Label("");
+	GUILayout.Label(playerName);
+	GUILayout.Label(playerProfilePicture);
+	GUILayout.Label("Health");
+	GUILayout.Label(playerHealth.ToString());
+	GUILayout.Label("");
+	GUILayout.Label("Hunger");
+	GUILayout.Label(playerHunger.ToString());
+	GUILayout.Label("");
+	GUILayout.Label("Fatigue");
+	GUILayout.Label(playerFatigue.ToString());
+	GUILayout.EndVertical();
+	GUILayout.FlexibleSpace();
+	GUILayout.BeginVertical();
+	GUILayout.Label("");
+	GUILayout.Label("Difficulty");
+	GUILayout.Label(difficultyChoice);
+	GUILayout.Label("");
+	GUILayout.Label("");
+	GUILayout.Label("Townspeople");
+	GUILayout.Label("");
+	GUILayout.Label("Healthy");
+	GUILayout.Label(healthyPopulation.ToString());
+	GUILayout.Label("");
+	GUILayout.Label("Sick");
+	GUILayout.Label(sickPopulation.ToString());
+	GUILayout.Label("");
+	GUILayout.Label("Deceased");
+	GUILayout.Label(deceasedPopulation.ToString());
+	
+	GUILayout.EndVertical();
+	GUILayout.EndHorizontal();
+}
+
+static function OptionsTab(){
+	GUILayout.BeginVertical();
+	//TODO: write this
+	GUILayout.EndVertical();
+}
+
+static function InventoryTab()
+{
+	var fishingPoleWeight : float = 5.0;
+	var gunWeight : float = 2.5;
+	var repairKitWeight : float = 10.0;
+	var foodWeight : float = 1.0;
+	var baitWeight : float = 0.4;
+	var bulletsWeight : float = 0.2;
+	var medicineWeight : float = 4.0;
+
+	var fishingPoleIcon = Resources.Load("fishingpole");
+	var gunIcon = Resources.Load("gun");
+	var repairKitIcon = Resources.Load("repairkit");
+	var foodIcon = Resources.Load("food");
+	var baitIcon = Resources.Load("bait");
+	var bulletsIcon = Resources.Load("bullets");
+	var medicineIcon = Resources.Load("medicine");
+	var sledgraphicIcon = Resources.Load("sledgraphic");
+	
+	var sledLoad : float = PlayerPrefs.GetFloat("SledLoad");
+	var sledCapacity : float = PlayerPrefs.GetFloat("SledCapacity");
+	var basketHealth : int = PlayerPrefs.GetInt("BasketHealth");
+	var runnerHealth : int = PlayerPrefs.GetInt("RunnerHealth");
+	var sledModifier : float = PlayerPrefs.GetFloat("SledModifier");
+	var repairKitCount : int = PlayerPrefs.GetInt("RepairKitCount");
+
+	GUILayout.BeginVertical();
+	
+	// labels.
+	GUILayout.BeginHorizontal();
+	GUILayout.FlexibleSpace();
+	GUILayout.Label("Name", GUILayout.Width(120));
+	GUILayout.Label("", GUILayout.Width(120));
+	GUILayout.Label("Quantity", GUILayout.Width(120));
+	GUILayout.Label("Total Weight", GUILayout.Width(120));
+	GUILayout.FlexibleSpace();
+	GUILayout.EndHorizontal();
+	
+	// fishing pole.
+	GUILayout.BeginHorizontal();
+	GUILayout.FlexibleSpace();
+	GUILayout.Label("Fishing Pole", GUILayout.Width(120));
+	GUILayout.Label(fishingPoleIcon, GUILayout.Width(120));
+	GUILayout.Label(PlayerPrefs.GetInt("FishingPoleCount").ToString(), GUILayout.Width(120));
+	GUILayout.Label((PlayerPrefs.GetInt("FishingPoleCount") * fishingPoleWeight).ToString(), GUILayout.Width(120));
+	GUILayout.FlexibleSpace();
+	GUILayout.EndHorizontal();
+	
+	// gun.
+	GUILayout.BeginHorizontal();
+	GUILayout.FlexibleSpace();
+	GUILayout.Label("Gun", GUILayout.Width(120));
+	GUILayout.Label(gunIcon, GUILayout.Width(120));
+	GUILayout.Label(PlayerPrefs.GetInt("GunCount").ToString(), GUILayout.Width(120));
+	GUILayout.Label((PlayerPrefs.GetInt("GunCount") * gunWeight).ToString(), GUILayout.Width(120));
+	GUILayout.FlexibleSpace();
+	GUILayout.EndHorizontal();
+	
+	// repair kits.
+	GUILayout.BeginHorizontal();
+	GUILayout.FlexibleSpace();
+	GUILayout.Label("Repair Kit", GUILayout.Width(120));
+	GUILayout.Label(repairKitIcon, GUILayout.Width(120));
+	GUILayout.Label(PlayerPrefs.GetInt("RepairKitCount").ToString(), GUILayout.Width(120));
+	GUILayout.Label((PlayerPrefs.GetInt("RepairKitCount") * repairKitWeight).ToString(), GUILayout.Width(120));
+	GUILayout.FlexibleSpace();
+	GUILayout.EndHorizontal();
+	
+	// food.
+	GUILayout.BeginHorizontal();
+	GUILayout.FlexibleSpace();
+	GUILayout.Label("Food", GUILayout.Width(120));
+	GUILayout.Label(foodIcon, GUILayout.Width(120));
+	GUILayout.Label(PlayerPrefs.GetInt("FoodCount").ToString(), GUILayout.Width(120));
+	GUILayout.Label((PlayerPrefs.GetInt("FoodCount") * foodWeight).ToString(), GUILayout.Width(120));
+	GUILayout.FlexibleSpace();
+	GUILayout.EndHorizontal();
+	
+	// bait.
+	GUILayout.BeginHorizontal();
+	GUILayout.FlexibleSpace();
+	GUILayout.Label("Bait", GUILayout.Width(120));
+	GUILayout.Label(baitIcon, GUILayout.Width(120));
+	GUILayout.Label(PlayerPrefs.GetInt("BaitCount").ToString(), GUILayout.Width(120));
+	GUILayout.Label((PlayerPrefs.GetInt("BaitCount") * baitWeight).ToString(), GUILayout.Width(120));
+	GUILayout.FlexibleSpace();
+	GUILayout.EndHorizontal();
+	
+	// bullets.
+	GUILayout.BeginHorizontal();
+	GUILayout.FlexibleSpace();
+	GUILayout.Label("Bullets", GUILayout.Width(120));
+	GUILayout.Label(bulletsIcon, GUILayout.Width(120));
+	GUILayout.Label(PlayerPrefs.GetInt("BulletsCount").ToString(), GUILayout.Width(120));
+	GUILayout.Label((PlayerPrefs.GetInt("BulletsCount") * bulletsWeight).ToString(), GUILayout.Width(120));
+	GUILayout.FlexibleSpace();
+	GUILayout.EndHorizontal();
+	
+	// medicine.
+	GUILayout.BeginHorizontal();
+	GUILayout.FlexibleSpace();
+	GUILayout.Label("Medicine", GUILayout.Width(120));
+	GUILayout.Label(medicineIcon, GUILayout.Width(120));
+	GUILayout.Label(PlayerPrefs.GetInt("MedicineCount").ToString(), GUILayout.Width(120));
+	GUILayout.Label((PlayerPrefs.GetInt("MedicineCount") * medicineWeight).ToString(), GUILayout.Width(120));
+	GUILayout.FlexibleSpace();
+	GUILayout.EndHorizontal();
+	
+	GUILayout.BeginHorizontal();
+	GUILayout.Label("");
+	GUILayout.EndHorizontal();
+	
+	GUILayout.BeginHorizontal();
+	GUILayout.FlexibleSpace();
+	GUILayout.Label(sledgraphicIcon);
+	GUILayout.FlexibleSpace();
+	GUILayout.EndHorizontal();
+	
+	GUILayout.BeginHorizontal();
+	GUILayout.FlexibleSpace();
+	GUILayout.Label("Sled Load: " + sledLoad.ToString("F1") + "/" + sledCapacity.ToString("F1"));
+	GUILayout.FlexibleSpace();
+	GUILayout.EndHorizontal();
+	
+	GUILayout.BeginHorizontal();
+	GUILayout.FlexibleSpace();
+	GUILayout.Label("Basket Health: " + basketHealth.ToString());
+	GUILayout.Label("", GUILayout.Width(10));
+	if(GUILayout.Button("Use Repair Kit", GUILayout.MaxWidth(120)))
+	{
+		if(basketHealth < 100 && repairKitCount > 0)
+		{
+			if(basketHealth == 0)
+			{
+				basketHealth += 10;
+				sledCapacity = 250.0;
+				PlayerPrefs.SetFloat("SledCapacity", sledCapacity);
+			}
+			
+			if(basketHealth <= 90)
+			{
+				basketHealth += 10;
+			}
+			
+			else
+			{
+				basketHealth = 100;
+			}
+			
+			repairKitCount--;
+			PlayerPrefs.SetInt("BasketHealth", basketHealth);
+			PlayerPrefs.SetInt("RepairKitCount", repairKitCount);
+			PlayerPrefs.SetFloat("SledLoad", sledLoad);
+		}
+	}
+	GUILayout.FlexibleSpace();
+	GUILayout.EndHorizontal();
+	
+	GUILayout.BeginHorizontal();
+	GUILayout.FlexibleSpace();
+	GUILayout.Label("Runner Health: " + runnerHealth.ToString());
+	GUILayout.Label("", GUILayout.Width(10));
+	if(GUILayout.Button("Use Repair Kit", GUILayout.MaxWidth(120)))
+	{
+		if(runnerHealth < 100 && repairKitCount > 0)
+		{
+			if(runnerHealth == 0)
+			{
+				runnerHealth += 10;
+				sledModifier = 1.0;
+				PlayerPrefs.SetFloat("SledModifier", sledModifier);
+			}
+		
+			if(runnerHealth <= 90)
+			{
+				runnerHealth += 10;
+			}
+			
+			else
+			{
+				runnerHealth = 100;
+			}
+			
+			repairKitCount--;
+			sledLoad -= repairKitWeight;
+			PlayerPrefs.SetInt("RepairKitCount", repairKitCount);
+			PlayerPrefs.SetInt("RunnerHealth", runnerHealth);
+			PlayerPrefs.SetFloat("SledLoad", sledLoad);
+		}
+	}
+	
+	GUILayout.FlexibleSpace();
+	GUILayout.EndHorizontal();
+	
+	GUILayout.EndVertical();
+}
+
+static function DogsTab()
+{
+	GUILayout.BeginVertical();
+	for(var i : int =0;i<PlayerPrefs.GetInt("DogCount");i++){
+		if(PlayerPrefs.GetInt("dogIsDead"+i)==0)// the dog isn't dead
+		{
+			// name
+			GUILayout.Label(PlayerPrefs.GetString("dogName"+i));
+			// health
+			GUILayout.BeginHorizontal();
+			GUILayout.Label("Health: ", GUILayout.Width(50));
+			GUILayout.Label(PlayerPrefs.GetInt("dogHealth"+i).ToString(), GUILayout.Width(50));
+			GUILayout.EndHorizontal();
+			
+			// fatigue
+			GUILayout.BeginHorizontal();
+			GUILayout.Label("Fatigue: ", GUILayout.Width(50));
+			GUILayout.Label(PlayerPrefs.GetInt("dogFatigue"+i).ToString(), GUILayout.Width(50));
+			GUILayout.EndHorizontal();
+			
+			// hunger
+			GUILayout.BeginHorizontal();
+			GUILayout.Label("Hunger: ", GUILayout.Width(50));
+			GUILayout.Label(PlayerPrefs.GetInt("dogHunger"+i).ToString(), GUILayout.Width(50));
+			GUILayout.EndHorizontal();
+			
+			GUILayout.Label(""); // space in between each dog
+		}
+	}
+	GUILayout.EndVertical();
+}	
+
+static function emptyWindow(){
+	GUILayout.BeginVertical();
+	GUILayout.EndVertical();
+}
